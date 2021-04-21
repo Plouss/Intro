ce49aa4235597d2053c88b67dcb0f34d1ffa5f68
Plouss <80962741+Plouss@users.noreply.github.com>
2021-04-21 22:12:47 +0200

    Initial commit

---
+++ public/js/main.js
@@ -0,0 +1,34 @@
+//type de variables 
+
+//type string
+let phrase = "Bonjour je suis cactus";
+console.log(phrase);
+//réassigner une variable
+phrase = "Bonjour je suis un hmar";
+console.log(phrase);
+//type integer
+let nombre = 2000; //let nombre = "2000" =/= 2000;
+nombre = 100;
+
+// console.log(typeof phrase); permet de vérifier quel type de variable c'est
+// console.log(typeof nombre);
+
+//type boolean
+let vrai = true;
+let faux = false;
+let resultat = 8 > 19;
+
+//type objet
+let object = {};
+
+//type array
+let array = [];
+
+//type undefined & null
+let vide;
+// console.log(vide);
+// console.log(typeof vide);
+
+let rien = null;
+// console.log(rien);
+// console.log(typeof rien);
